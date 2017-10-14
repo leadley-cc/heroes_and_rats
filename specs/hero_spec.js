73d93780b928a435540120b6bb5509ff5fa5bca5
Michael Leadley <leadley75@gmail.com>
2017-10-14 15:10:04 +0100

    Add tests for Hero eat function

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,11 +1,16 @@
 const assert = require("assert")
 const Hero = require("../hero")
+const Food = require("../food")
 
 describe("Hero", () => {
   var hero
+  var apple
+  var cheese
 
   beforeEach(() => {
     hero = new Hero("Michael", "cheese")
+    apple = new Food("apple", 4)
+    cheese = new Food("cheese", 6)
   })
 
   it("should have a name", () => {
@@ -26,5 +31,15 @@ describe("Hero", () => {
 
   it("should start with an empty quest log", () => {
     assert.deepStrictEqual(hero.questLog, [])
+  })
+
+  it("should be able to eat food to replenish health", () => {
+    hero.eat(apple)
+    assert.strictEqual(hero.health, 24)
+  })
+
+  it("should be able to eat their fave food for 1.5x health", () => {
+    hero.eat(cheese)
+    assert.strictEqual(hero.health, 29)
   })
 })
